813292d12ed2254426e4478208499e7fdea81029
g0j0 H4R5H <notsoharsh13@gmail.com>
2026-04-09 18:07:21 +0530

    Site updated: 2026-04-09 18:07:19

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1757431371895";const preCache = ["/images/taichi.png","/images/gojo_cigg.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1775738230263";const preCache = ["/images/taichi.png","/images/gojo_cigg.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
